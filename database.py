5e7e11fa70f30da14b32b28ad634005f704ebb0c
projectskl17 <hermit0bot@gmail.com>
2024-11-05 23:27:03 +0530

    Update database.py

--- database.py
+++ database.py
@@ -77,7 +77,7 @@ class Database:
     async def get_configs(self, id):
         default = {
             'delete_time': 0,
-            'batch': 0,
+            'batch': 100,
             'caption': None,
             'duplicate': True,
             'forward_tag': False,
